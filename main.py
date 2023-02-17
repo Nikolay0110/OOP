163662e9fd4e6906fc1156ceb25d6ac5802a2ca8
Nikolay <kolunya0@gmail.com>
2023-02-17 17:16:12 +0300

    step 2 complite

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 class Student:
     def __init__(self, name, surname, gender):
+        self.courses_attached = []
         self.name = name
         self.surname = surname
         self.gender = gender
@@ -9,6 +10,16 @@ class Student:
 
     def add_courses(self, course_name):
         self.finished_courses.append(course_name)
+
+    def rate_lecturer(self, lecturer, course, grade):
+        if isinstance(lecturer,
+                      Lecturer) and course in self.courses_attached:
+            if course in lecturer.grades:
+                lecturer.grades[course] += [grade]
+            else:
+                lecturer.grades[course] = [grade]
+        else:
+            return 'Ошибка'
 
 
 class Mentor:
@@ -24,14 +35,20 @@ class Mentor:
             else:
                 student.grades[course] = [grade]
         else:
-            return 'Ошибка'
+            return "Ошибка"
 
 
 class Lecturer(Mentor):
-    pass
+    def __init__(self, name, surname):
+        super().__init__(name, surname)
+        self.grades = {}
+
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname} \nСредняя оценка за лекции: '
+
 
 class Reviewer(Mentor):
-    pass
+
     def rate_hw(self, student, course, grade):
         if isinstance(student, Student) and course in self.courses_attached and course in student.courses_in_progress:
             if course in student.grades:
@@ -40,6 +57,9 @@ class Reviewer(Mentor):
                 student.grades[course] = [grade]
         else:
             return 'Ошибка'
+
+    def __str__(self):
+        return f'Имя: {self.name} \nФамилия: {self.surname}'
 
 
 best_student = Student('Ruoy', 'Eman', 'your_gender')
